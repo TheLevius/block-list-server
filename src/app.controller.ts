98355326df2238c1dbb09633c53ce4f0e94f2944
Nikita Levitski <levius@Nikitas-MacBook-Air.local>
2023-11-18 23:02:13 +0300

    feat: prisma db service and swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
+import { DbService } from './db/db.service';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
 
+class HelloWorldDto {
+  @ApiProperty()
+  message: string;
+}
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dbService: DbService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({ type: HelloWorldDto })
+  async getHello(): Promise<HelloWorldDto> {
+    const users = await this.dbService.user.findMany({});
+    console.log(users);
+    return { message: this.appService.getHello() };
   }
 }
